42b706dbb4e6453f943687aa1cbed3cb3ce17366
Ignacio Consuegra <ignacioandresconsuegradelacruz@gmail.com>
2025-11-06 09:10:55 -0500

    Generated stars, not movement yet

--- fronted/Pages/Home.jsx
+++ fronted/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import ThemeToggle from "../Components/ThemeToggle"
+import ThemeToggle from "../Components/ThemeToggle";
+import {StarBackground} from "../Components/StarBackground"
 const Home = () => {
   return (
     <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
       <ThemeToggle/>
+      <StarBackground/>
     </div>
   )
 }
