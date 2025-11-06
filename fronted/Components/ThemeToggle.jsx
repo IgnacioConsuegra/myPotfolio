50ae2a32a262a9ef4b41478bd812ab04a922adce
Ignacio Consuegra <ignacioandresconsuegradelacruz@gmail.com>
2025-11-06 10:56:54 -0500

    NavBarFinished

--- fronted/Components/ThemeToggle.jsx
+++ fronted/Components/ThemeToggle.jsx
@@ -1,32 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { Moon, Sun } from "lucide-react";
 import { cn } from "../lib/utils";
-const ThemeToggle = () => {
+
+export const ThemeToggle = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme === "dark") {
+      setIsDarkMode(true);
+      document.documentElement.classList.add("dark");
+    } else {
+      localStorage.setItem("theme", "light");
+      setIsDarkMode(false);
+    }
+  }, []);
+
   const toggleTheme = () => {
     if (isDarkMode) {
-      setIsDarkMode(false);
       document.documentElement.classList.remove("dark");
       localStorage.setItem("theme", "light");
+      setIsDarkMode(false);
     } else {
       document.documentElement.classList.add("dark");
       localStorage.setItem("theme", "dark");
       setIsDarkMode(true);
     }
   };
-  useEffect(() => {
-    const storedTheme = localStorage.getItem("theme");
-    if (storedTheme === "dark") {
-      setIsDarkMode("true");
-      document.documentElement.classList.add("dark");
-    } else {
-      setIsDarkMode(false);
-      localStorage.setItem("theme", "light");
-    }
-  }, []);
+
   return (
-    <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300", 
-    "focus:outline-hidden" )}>
+    <button
+      onClick={toggleTheme}
+      className={cn(
+        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300  ",
+        "focus:outlin-hidden hover:cursor-pointer"
+      )}
+    >
       {isDarkMode ? (
         <Sun className="h-6 w-6 text-yellow-300" />
       ) : (
@@ -35,4 +44,3 @@ const ThemeToggle = () => {
     </button>
   );
 };
-export default ThemeToggle;
--- fronted/Pages/Home.jsx
+++ fronted/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import ThemeToggle from "../Components/ThemeToggle";
-import {StarBackground} from "../Components/StarBackground"
+import {ThemeToggle} from "../Components/ThemeToggle";
+import {StarBackground} from "../Components/StarBackground";
+import { NavBar } from '../Components/NavBar';
 const Home = () => {
   return (
-    <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       <ThemeToggle/>
       <StarBackground/>
+      <NavBar/>
     </div>
   )
 }
