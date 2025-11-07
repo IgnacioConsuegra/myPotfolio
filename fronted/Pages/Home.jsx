43964153f283824228abce8574be102bab5187d6
Ignacio Consuegra <ignacioandresconsuegradelacruz@gmail.com>
2025-11-07 08:20:44 -0500

    Hero section finished

--- fronted/Pages/Home.jsx
+++ fronted/Pages/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import {ThemeToggle} from "../Components/ThemeToggle";
 import {StarBackground} from "../Components/StarBackground";
 import { NavBar } from '../Components/NavBar';
+import { HeroSection } from '../Components/HeroSection';
 const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       <ThemeToggle/>
       <StarBackground/>
       <NavBar/>
+      <main>
+        <HeroSection/>
+      </main>
     </div>
   )
 }
