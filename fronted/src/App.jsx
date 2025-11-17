9a4e50e6c1c7e0a66ead1c911b04a1f53182607a
Ignacio Consuegra <ignacioandresconsuegradelacruz@gmail.com>
2025-11-17 07:47:30 -0500

    added fetch to my projects

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../Pages/Home";
 import NotFound from "../Pages/NotFound";
 function App() {
+  useEffect(() => {
+    const blogFetch = fetch("https://blog-evtq.onrender.com/");
+    const shirtFetch = fetch("https://threed-shirt-1.onrender.com/");
+    const chatFetch = fetch("https://real-time-chat-1-icd4.onrender.com/");
+  }, [])
   return (
     <main>
       <Routes>
